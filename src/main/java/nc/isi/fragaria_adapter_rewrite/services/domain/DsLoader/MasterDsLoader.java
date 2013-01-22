fcb0780d86671ca1d93c610961bec5f1e5405685
jmaltat <jmaltat@isi.nc>
2013-01-22 14:20:18 +1100

    modification du pom

--- src/main/java/nc/isi/fragaria_adapter_rewrite/services/domain/DsLoader/MasterDsLoader.java
+++ src/main/java/nc/isi/fragaria_adapter_rewrite/services/domain/DsLoader/MasterDsLoader.java
@@ -3,14 +3,17 @@ package nc.isi.fragaria_adapter_rewrite.services.domain.DsLoader;
 import java.util.Map;
 
 import nc.isi.fragaria_adapter_rewrite.services.domain.Datasource;
+
 /**
  * 
- * @author bjonathas
- * Service permettant de charger toutes des datasources à utiliser dans le projet.
- * Le chargement s'effectue grace à des services plus spécifics permettant de construir ces datasources à 
- * partir de différentes sources de données (ex : fichier.yaml). Pour rajouter une source de données, il suffit
- * de contribuer à ce service le specificDsLoader correspondant à la source de données (ex : yamlDsLoader)
+ * @author bjonathas Service permettant de charger toutes des datasources à
+ *         utiliser dans le projet. Le chargement s'effectue grace à des
+ *         services plus spécifics permettant de construir ces datasources à
+ *         partir de différentes sources de données (ex : fichier.yaml). Pour
+ *         rajouter une source de données, il suffit de contribuer à ce service
+ *         le specificDsLoader correspondant à la source de données (ex :
+ *         yamlDsLoader)
  */
 public interface MasterDsLoader {
-	Map<String,Datasource> getDs();
+	Map<String, Datasource> getDs();
 }
--- src/main/java/nc/isi/fragaria_adapter_rewrite/services/domain/DsLoader/MasterDsLoaderImpl.java
+++ src/main/java/nc/isi/fragaria_adapter_rewrite/services/domain/DsLoader/MasterDsLoaderImpl.java
@@ -5,21 +5,20 @@ import java.util.Map;
 
 import nc.isi.fragaria_adapter_rewrite.services.domain.Datasource;
 
-import com.beust.jcommander.internal.Lists;
 import com.google.common.collect.Maps;
 
 public class MasterDsLoaderImpl implements MasterDsLoader {
 
-	List<SpecificDsLoader> list = Lists.newArrayList();
-	
+	private final List<SpecificDsLoader> list;
+
 	public MasterDsLoaderImpl(List<SpecificDsLoader> list) {
 		this.list = list;
 	}
-	
+
 	@Override
 	public Map<String, Datasource> getDs() {
-		Map<String,Datasource> map = Maps.newHashMap();
-		for(SpecificDsLoader loader :  list)
+		Map<String, Datasource> map = Maps.newHashMap();
+		for (SpecificDsLoader loader : list)
 			map.putAll(loader.getDs());
 		return map;
 	}
