37854d7db3a64c7338180aa5cec1aa982ee696e4
jmaltat <jmaltat@isi.nc>
2013-02-13 14:13:29 +1100

    helper pour les get simples

---
+++ src/main/java/nc/isi/fragaria_adapter_rewrite/entities/AbstractEntityGetterHelper.java
@@ -0,0 +1,44 @@
+package nc.isi.fragaria_adapter_rewrite.entities;
+
+import java.math.BigDecimal;
+
+
+import org.joda.time.DateTime;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+public abstract class AbstractEntityGetterHelper extends AbstractEntity {
+
+	public AbstractEntityGetterHelper() {
+		super();
+	}
+
+	public AbstractEntityGetterHelper(ObjectNode objectNode) {
+		super(objectNode);
+	}
+
+	public DateTime readDateTimeProperty(String propertyName) {
+		return readProperty(DateTime.class, propertyName);
+	}
+
+	public Integer readIntegerProperty(String propertyName) {
+		return readProperty(Integer.class, propertyName);
+	}
+
+	public Double readDoubleProperty(String propertyName) {
+		return readProperty(Double.class, propertyName);
+	}
+
+	public BigDecimal readBigDecimalProperty(String propertyName) {
+		return readProperty(BigDecimal.class, propertyName);
+	}
+
+	public String readStringProperty(String propertyName) {
+		return readProperty(String.class, propertyName);
+	}
+
+	public Boolean readBooleanProperty(String propertyName) {
+		return readProperty(Boolean.class, propertyName);
+	}
+
+}
