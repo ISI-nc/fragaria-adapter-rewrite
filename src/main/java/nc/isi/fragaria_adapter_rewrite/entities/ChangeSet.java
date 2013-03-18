c77ed4fe7bca8ec0e5b5e1b538eb4a4d070ab7f8
Mikaël Cluseau <mcluseau@isi.nc>
2013-03-18 21:49:44 +1100

    Généralisation vers ChangeSet pour améliorer la réutilisabilité.

---
+++ src/main/java/nc/isi/fragaria_adapter_rewrite/entities/ChangeSet.java
@@ -0,0 +1,225 @@
+package nc.isi.fragaria_adapter_rewrite.entities;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+/**
+ * Classe représentant un ensemble de changements.
+ * 
+ * @author Mikaël Cluseau
+ * 
+ */
+public class ChangeSet<K extends Comparable<? super K>> {
+
+	private final Map<K, JsonNode> modifications = new TreeMap<>();
+
+	private final Set<K> deletions = new TreeSet<>();
+
+	// ------------------------------------------------------------------
+	// Status methods
+	//
+
+	/**
+	 * @param fieldName
+	 *            The name of the field we're looking for.
+	 * @return <code>true</code> iff <code>fieldName</code> is modified or
+	 *         removed.
+	 */
+	public boolean isChanged(K fieldName) {
+		return isModified(fieldName) || isRemoved(fieldName);
+	}
+
+	/**
+	 * @return All changed field (modified of removed).
+	 */
+	public Set<K> changes() {
+		Set<K> changes = new TreeSet<>();
+		changes.addAll(modifiedFields());
+		changes.addAll(removedFields());
+		return changes;
+	}
+
+	/**
+	 * @param fieldName
+	 *            The name of the field we're looking for.
+	 * @return <code>true</code> iff it has been modified (not removed).
+	 */
+	public boolean isModified(K fieldName) {
+		return modifications.containsKey(fieldName);
+	}
+
+	/**
+	 * @return All modified fields.
+	 */
+	public Set<K> modifiedFields() {
+		Set<K> modifiedFields = new TreeSet<>();
+		for (K fieldName : modifications.keySet()) {
+			modifiedFields.add(fieldName);
+		}
+		return Collections.unmodifiableSet(modifiedFields);
+	}
+
+	/**
+	 * 
+	 * @param fieldName
+	 *            The name of the field we're looking for.
+	 * @return <code>true</code> iff it has been removed (not modified).
+	 */
+	public boolean isRemoved(K fieldName) {
+		return deletions.contains(fieldName);
+	}
+
+	/**
+	 * @return All removed fields.
+	 */
+	public Set<K> removedFields() {
+		return Collections.unmodifiableSet(deletions);
+	}
+
+	// ------------------------------------------------------------------
+	// State change methods
+	//
+
+	/**
+	 * Retrieves the modified value for a field.
+	 * 
+	 * @param fieldName
+	 *            The name of the field we're looking for.
+	 * @return The new value for this field.
+	 */
+	public JsonNode get(K fieldName) {
+		if (isModified(fieldName)) {
+			return modifications.get(fieldName);
+		} else if (isRemoved(fieldName)) {
+			return null;
+		} else {
+			throw new NoSuchElementException("Field " + fieldName
+					+ " has not changed.");
+		}
+	}
+
+	/**
+	 * Records the modification of a field.
+	 * 
+	 * @param fieldName
+	 *            The name of the modified field.
+	 * @param newValue
+	 *            The new value set.
+	 */
+	public void modify(K fieldName, JsonNode newValue) {
+		modifications.put(fieldName, newValue);
+		// Si cette valeur était supprimée, elle ne l'est plus.
+		deletions.remove(fieldName);
+	}
+
+	/**
+	 * Records the removal of a field.
+	 * 
+	 * @param fieldName
+	 *            The removed field.
+	 */
+	public void remove(K fieldName) {
+		deletions.add(fieldName);
+		// Si cette valeur était modifiée, elle n'est plus.
+		if (modifications.containsKey(fieldName)) {
+			modifications.remove(fieldName);
+		}
+	}
+
+	/**
+	 * Clear the changes recorded by this change set.
+	 */
+	public void clear() {
+		modifications.clear();
+		deletions.clear();
+	}
+
+	// ------------------------------------------------------------------
+	// ChangeSet level methods
+	//
+
+	/**
+	 * Merges the changes from another {@link ChangeSet}.
+	 * 
+	 * @param other
+	 *            The {@link ChangeSet} to merge with.
+	 */
+	public void mergeWith(ChangeSet<K> other) throws MergeConflictException {
+		mergeWith(other, ConflictResolution.FAIL);
+	}
+
+	/**
+	 * Merges the changes from another {@link ChangeSet}.
+	 * 
+	 * @param other
+	 *            The {@link ChangeSet} to merge with.
+	 * @param resolution
+	 *            The resolution to apply in the event of a conflict.
+	 */
+	public void mergeWith(ChangeSet<K> other, ConflictResolution resolution)
+			throws MergeConflictException {
+		Set<K> modifiedFields = other.modifiedFields();
+		// Look for merge conflicts
+		if (resolution == ConflictResolution.FAIL) {
+			Set<K> conflictingKeys = new TreeSet<>();
+			for (K modifiedField : modifiedFields) {
+				if (!isModified(modifiedField)) {
+					continue;
+				}
+				JsonNode mine = get(modifiedField);
+				JsonNode their = other.get(modifiedField);
+				if (nullSafeEquals(mine, their)) {
+					continue; // modified by the same value => no conflict
+				}
+				conflictingKeys.add(modifiedField);
+			}
+			if (!conflictingKeys.isEmpty()) {
+				throw new MergeConflictException(conflictingKeys);
+			}
+		}
+		// Merge modifications
+		for (K modifiedField : modifiedFields) {
+			if (isModified(modifiedField)) {
+				JsonNode mine = get(modifiedField);
+				JsonNode their = other.get(modifiedField);
+				if (nullSafeEquals(mine, their)) {
+					continue; // modified by the same value => ignore
+				}
+				// Conflict
+				switch (resolution) {
+				case OURS:
+					// ignore other's change
+					break;
+				case THEIRS:
+					// overwrite our change
+					modify(modifiedField, their);
+					break;
+				case FAIL:
+					throw new IllegalStateException("Unreachable code");
+				default:
+					throw new IllegalArgumentException("Unknown resolution: "
+							+ resolution);
+				}
+			} else if (isRemoved(modifiedField)) {
+				// removed on our side, so ignore the other's change
+			} else {
+				modify(modifiedField, other.get(modifiedField));
+			}
+		}
+		// Merge removals
+		for (K removedField : other.removedFields()) {
+			remove(removedField);
+		}
+	}
+
+	private boolean nullSafeEquals(Object mine, Object their) {
+		return mine == null ? their == null : mine.equals(their);
+	}
+
+}
--- src/main/java/nc/isi/fragaria_adapter_rewrite/entities/MergeConflictException.java
+++ src/main/java/nc/isi/fragaria_adapter_rewrite/entities/MergeConflictException.java
@@ -8,18 +8,21 @@ import java.util.TreeSet;
 public class MergeConflictException extends Exception {
 	private static final long serialVersionUID = 6827579801273073832L;
 
-	private final Set<String> conflictingKeys;
+	private final Set<?> conflictingKeys;
 
-	public MergeConflictException(String... conflictingKeys) {
+	@SafeVarargs
+	public <K extends Comparable<? super K>> MergeConflictException(
+			K... conflictingKeys) {
 		this(Arrays.asList(conflictingKeys));
 	}
 
-	public MergeConflictException(Collection<String> conflictingKeys) {
+	public <K extends Comparable<? super K>> MergeConflictException(
+			Collection<K> conflictingKeys) {
 		super("Conflict on the following keys: " + conflictingKeys);
-		this.conflictingKeys = new TreeSet<>(conflictingKeys);
+		this.conflictingKeys = new TreeSet<K>(conflictingKeys);
 	}
 
-	public Set<String> getConflictingKeys() {
+	public Set<?> getConflictingKeys() {
 		return conflictingKeys;
 	}
 
