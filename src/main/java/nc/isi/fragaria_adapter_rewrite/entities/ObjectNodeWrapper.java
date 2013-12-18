02009a9a98d79a45f3df228ac7e3c07559ce4af2
brounchy <benoit.jonathas@gmail.com>
2013-12-18 16:12:17 +1100

    dont resolve if there is no backReference

--- src/main/java/nc/isi/fragaria_adapter_rewrite/entities/ObjectNodeWrapper.java
+++ src/main/java/nc/isi/fragaria_adapter_rewrite/entities/ObjectNodeWrapper.java
@@ -48,11 +48,9 @@ public abstract class ObjectNodeWrapper implements Entity {
 		if (node.has(metadata().getJsonPropertyName(propertyName))) {
 			return resolveFromNode(propertyType, propertyName);
 		} else {
-			if ((getState().equals(State.NEW) || (!getState().equals(State.NEW) && !toJSON().toString().equals("{\"_id\":\""+getId()+"\"}")  )) && (!Entity.class.isAssignableFrom(metadata().propertyType(
-					propertyName))
-					|| (metadata().getCollectionType(propertyName) != null && !Entity.class
-							.isAssignableFrom(metadata().getCollectionType(
-									propertyName)))))
+			if ((getState().equals(State.NEW) || (!getState().equals(State.NEW) && !toJSON()
+					.toString().equals("{\"_id\":\"" + getId() + "\"}")))
+					&& metadata().getBackReference(propertyName) == null)
 				return null;
 			// Object is new
 			if (propertyName.equals(Entity.ID)) {
